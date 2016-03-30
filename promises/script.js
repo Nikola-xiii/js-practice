ab8655e9a5f765a90d3ca98dc9f99e3b8d0ec84b
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2016-03-30 13:49:49 +0300

    #1: promise dev.mozilla example done

--- promises/script.js
+++ promises/script.js
@@ -7,24 +7,24 @@ function testPromise() {
   var thisPromiseCount = ++promiseCount;
 
   var log = document.getElementById('log');
-  log.insertAdjacentHTML('beforeeed', thisPromiseCount + ') Start (Sync code)');
+  log.insertAdjacentHTML('beforeEnd', thisPromiseCount + ') Start (Sync code)');
   
   var p1 = new Promise (
     function (resolve, reject) {
-      log.insertAdjacentHTML('beforeend', thisPromiseCount + ') Start alert (запуск Async code)');
+      log.insertAdjacentHTML('beforeEnd', thisPromiseCount + ') Start alert (запуск Async code)');
       window.setTimeout(
         function () {
           resolve(thisPromiseCount);
-        }, Math.random() * 2000 + 1000
+        }, Math.random() * 5000 + 1000
       );
     }
   );
 
   p1.then(
     function (val) {
-      log.insertAdjacentHTML('beforeend', val + ') Done alert (запуск Async code)');
+      log.insertAdjacentHTML('beforeEnd', val + ') Done alert (запуск Async code)');
     }
   )
 
-  log.insertAdjacentHTML('beforeeed', thisPromiseCount + ') Create (Sync code)');
+  log.insertAdjacentHTML('beforeEnd', thisPromiseCount + ') Create alert (Sync code)');
 }
