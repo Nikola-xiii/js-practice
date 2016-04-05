a879d8b80b9188da0e7700f63e28129d53e24416
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2016-04-05 21:51:28 +0300

    #2: Immutable list

--- immutable/script.js
+++ immutable/script.js
@@ -3,7 +3,15 @@
  */
 var Immutable = require('immutable');
 
+var list = Immutable.List.of(1,1,34,5);
+
 var map = Immutable.Map({"_id": 1, "student": "Mary", "grade": 45, "assignment": "homework"});
 
 map = map.set("grade", 48);
-console.log(map);
+
+var list2 = list.withMutations(function (list) {
+  list.push(4).push(5).push(6);
+});
+
+console.log(list2);
+
