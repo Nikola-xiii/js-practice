4db595a3e609eb63241a2feed2d1ed68b261900d
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2016-04-19 22:30:27 +0300

    #5: All Design Pattern

--- design-patterns/script.js
+++ design-patterns/script.js
@@ -1,3 +1,5 @@
+
+// TODO : Module Design Pattern
 (function() {
 
   // declare private variables and/or functions
@@ -7,3 +9,130 @@
   }
 
 })();
+
+// TODO : Prototype Design Pattern
+
+var TeslaModelS = function() {
+  this.numWheels    = 4;
+  this.manufacturer = 'Tesla';
+  this.make         = 'Model S';
+};
+
+TeslaModelS.prototype = function() {
+
+  var go = function() {
+    // Rotate wheels
+    console.log("GO");
+  };
+
+  var stop = function() {
+    // Apply brake pads
+  };
+
+  return {
+    pressBrakePedal: stop,
+    pressGasPedal: go
+  }
+
+}();
+
+// TODO : Observer Design Pattern
+
+var Subject = function() {
+  this.observers = [];
+
+  return {
+    subscribeObserver: function(observer) {
+      this.observers.push(observer);
+    },
+    unsubscribeObserver: function(observer) {
+      var index = this.observers.indexOf(observer);
+      if(index > -1) {
+        this.observers.splice(index, 1);
+      }
+    },
+    notifyObserver: function(observer) {
+      var index = this.observers.indexOf(observer);
+      if(index > -1) {
+        this.observers[index].notify(index);
+      }
+    },
+    notifyAllObservers: function() {
+      for(var i = 0; i < this.observers.length; i++){
+        this.observers[i].notify(i);
+      };
+    }
+  };
+};
+
+var Observer = function() {
+  return {
+    notify: function(index) {
+      console.log("Observer " + index + " is notified!");
+    }
+  }
+}
+
+var subject = new Subject();
+
+var observer1 = new Observer();
+var observer2 = new Observer();
+var observer3 = new Observer();
+var observer4 = new Observer();
+
+subject.subscribeObserver(observer1);
+subject.subscribeObserver(observer2);
+subject.subscribeObserver(observer3);
+subject.subscribeObserver(observer4);
+
+subject.notifyObserver(observer2); // Observer 2 is notified!
+
+subject.notifyAllObservers();
+// Observer 1 is notified!
+// Observer 2 is notified!
+// Observer 3 is notified!
+// Observer 4 is notified!
+
+
+// TODO : Singleton
+
+var printer = (function () {
+
+  var printerInstance;
+
+  function create () {
+
+    function print() {
+      // underlying printer mechanics
+    }
+
+    function turnOn() {
+      // warm up
+      // check for paper
+    }
+
+    return {
+      // public + private states and behaviors
+      print: print,
+      turnOn: turnOn
+    };
+  }
+
+  return {
+    getInstance: function() {
+      if(!printerInstance) {
+        printerInstance = create();
+      }
+      return printerInstance;
+    }
+  };
+
+  function Singleton () {
+    if(!printerInstance) {
+      printerInstance = intialize();
+    }
+  };
+
+})();
+
+var officePrinter = printer.getInstance();
